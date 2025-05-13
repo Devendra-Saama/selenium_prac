8142488ca017f0329d9b72a1b042a80535683f8a
Devendra-Saama <devendra.chaudhari@saama.com>
2025-05-13 11:34:37 +0530

    new line added

--- main/java/GetPassword.java
+++ main/java/GetPassword.java
@@ -17,6 +17,7 @@ public class GetPassword
         Thread.sleep(2000);
         driver.findElement(By.xpath("//*[@type='password']")).sendKeys(password);
         driver.findElement(By.xpath("//button[@type='submit']")).click();
+        System.out.println("Hello");
     }
 
     public static String getPassword(WebDriver driver) throws InterruptedException {
